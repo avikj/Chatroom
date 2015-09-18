cbd0000876eeeb9d4720d95de29f971c4df8d833
Avik Jain <avikjain12345@gmail.com>
2015-09-17 19:17:21 -0700

    Connected to Node.js server

--- app/src/main/java/com/avik/chatroom/MainActivity.java
+++ app/src/main/java/com/avik/chatroom/MainActivity.java
@@ -2,37 +2,62 @@ package com.avik.chatroom;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import io.socket.client.*;
 public class MainActivity extends Activity {
-
+    private Socket socket;
+    private EditText nickField;
+    private Button join;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       // SocketIO socket;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        nickField = (EditText)findViewById(R.id.userField);
+        join = (Button)findViewById(R.id.join);
+        try{
+            socket = IO.socket("http://10.0.0.33:3000");
+            socket.connect();
+        }catch(Exception e){
+            Log.d("error", "couldnt create socket");
+            e.printStackTrace();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
+    }
+
+    public void tryToJoin(View v){
+        Log.d("Log", "trying to join");
+        String name = nickField.getText().toString();
+        socket.emit("new user", name, new Ack() {
+            @Override
+            public void call(Object... args) {
+                Log.d("Log", "Callback");
+                for (int i = 0; i < args.length; i++) {
+                    Toast.makeText(MainActivity.this, args[i].toString(), Toast.LENGTH_SHORT).show();
+                    Log.d("arg", args[i].toString());
+                }
+            }
+        });
+       /* socket.emit("new user", name, function(data){
+            if(data.valid){
+                $('#nickWrap').hide();
+                $('#contentWrap').show();
+                $('#yourname').html('You are logged in as <b>'+name+'</b>');
+            }else{
+                $nickError.html('That username is taken! Try again.');
+            }
+        });
+        $nickBox.val('');*/
     }
 }
